bfb64c089a7bd1bed3f953ef7f9d202d6e622d48
Irene Mandayani <irenemandayani@students.amikom.ac.id>
2025-10-14 14:02:55 +0700

    ubah font

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin | Sintesa')</title>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;600;700&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
     <link rel="icon" href="{{ asset('images/skaduta_logo.png') }}" type="image/png"/>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <style>
-
-    {{-- Link untuk Favicon --}}
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    
+    <style>
+        /* Hapus semua tag <link> dan komentar dari sini */
         body {
+            /* Pastikan penulisan nama font benar */
             font-family: 'Poppins', Tahoma, Geneva, Verdana, sans-serif;
             margin: 0;
             background-color: #f4f7f6; /* Warna latar belakang umum */
