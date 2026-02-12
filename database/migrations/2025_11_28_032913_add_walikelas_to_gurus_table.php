3b10c8c92495cc109f19266d7a1ec61f8a65839c
tirtahakimpambudhi <awebthp@gmail.com>
2026-02-12 03:33:42 +0000

    change seeder for seed user table and, add migration guru table

--- database/migrations/2025_11_28_032913_add_walikelas_to_gurus_table.php
+++ database/migrations/2025_11_28_032913_add_walikelas_to_gurus_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,15 +7,23 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('guru', function (Blueprint $table) {
-            $table->string('walikelas')->nullable()->after('role');
+        Schema::create('guru', function (Blueprint $table) {
+            $table->string('nip')->primary(); 
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('no_telp')->nullable();
+            $table->text('alamat')->nullable(); 
+            $table->string('mata_pelajaran')->nullable(); 
+            $table->enum('role', ['guru_bk', 'kesiswaan', 'walikelas', 'admin', 'guru'])->default('guru');
+            $table->string('walikelas')->nullable(); 
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::table('guru', function (Blueprint $table) {
-            $table->dropColumn('walikelas');
-        });
+        Schema::dropIfExists('guru');
     }
 };
