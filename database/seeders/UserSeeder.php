513a53c115b1a4808dfee0103e2ba14b50e5c78a
tirtahakimpambudhi <awebthp@gmail.com>
2026-02-12 15:24:51 +0700

    add siswa seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Guru;
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use App\Models\Siswa;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -49,5 +49,26 @@ class UserSeeder extends Seeder
             'password' => Hash::make('guru123'),
             'role' => 'guru',
         ]);
+
+        // Pengguna Siswa
+
+            Siswa::create([
+                'nis' => '343434343434',
+                'nisn' => rand(1000000000, 9999999999),
+                'nama_lengkap' => fake()->name(),
+                'email' => fake()->unique()->safeEmail(),
+                'no_whatsapp' => '08' . rand(1000000000, 9999999999),
+                'rombel' => 'X-' . rand(1, 5),
+                'jurusan' => fake()->randomElement(['RPL', 'TKJ', 'AKL', 'BDP']),
+                'tempat_lahir' => fake()->city(),
+                'tanggal_lahir' => fake()->date(),
+                'jenis_kelamin' => fake()->randomElement(['L', 'P']),
+                'agama' => fake()->randomElement(['Islam', 'Kristen', 'Hindu', 'Budha']),
+                'nama_ortu' => fake()->name(),
+                'alamat' => fake()->address(),
+                'foto' => null,
+                'password' => Hash::make('password123'),
+                'is_default_password' => true,
+            ]);
     }
 }
